2fa1614a72bd7bc05f096d3adb8ef832ee713cbe
BrunoSampaioDev <sampaionascimento100@gmail.com>
2020-12-10 23:28:03 -0300

    remove 013

--- 013/infla/index.js
+++
@@ -1,8 +0,0 @@
-/* Function responsavel por fazer os calculos */
-function calculaInflacao(valorInicial, valorFinal){
-    
-    return (valorFinal - valorInicial) / valorInicial * 100;
-}
-
-console.log(`A inflação no período foi de ${calculaInflacao(3.30, 4.50).toFixed(2)}% 
-A estimativa de inflação diária á de ${(calculaInflacao(3.30, 4.50) / 6) .toFixed(2)}%`);
