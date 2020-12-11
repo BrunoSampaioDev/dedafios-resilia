037b69286ea0825184a6b446eb5f8967431517ee
BrunoSampaioDev <sampaionascimento100@gmail.com>
2020-12-10 23:23:44 -0300

    exercicio 14

---
+++ 14/lampada/index.js
@@ -0,0 +1,15 @@
+let lampadaNaoPlugada = false;
+let bulboQueimado = false;
+
+if (lampadaNaoPlugada) {
+
+    console.log('plugar a lampada!');
+
+} else {
+
+    if (bulboQueimado) {
+        console.log('trque o bulbo');
+    } else {
+        console.log('compre um nova lampada.');
+    }
+}
