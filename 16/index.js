53051b6214298014276dadbf0c7aec7a8137b695
BrunoSampaioDev <sampaionascimento100@gmail.com>
2020-12-15 00:10:46 -0300

    delete diplicate

--- 16/index.js
+++
@@ -1,9 +0,0 @@
-let btn = ['Bruno', 'Rafa', 'Paulo', 'Carlos'];
-
-function percorreArray() {
-    for (let i = 0; i < btn.length; i++) {
-
-        console.log(btn[i]);
-    }
-};
-percorreArray();
