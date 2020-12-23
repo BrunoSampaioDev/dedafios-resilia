255e9fbec656fb74df9877fc030973d3868fe0bd
BrunoSampaioDev <sampaionascimento100@gmail.com>
2020-12-23 00:04:46 -0300

    add 22

--- 22/btnNomes/index.js
+++ 22/btnNomes/index.js
@@ -2,11 +2,12 @@ var nomes = ['Giulia', 'Ana Vitória', 'Peter', 'Bruno'];
 
 
 nomes.forEach(e => {
+    var nomeFront = document.getElementById('nome-front')
     var btn = document.createElement('button');
     btn.textContent = e;
 
     btn.addEventListener('click', () => {
-        alert(e);
+        nomeFront.innerHTML = e;
     });
     document.body.append(btn);
 
